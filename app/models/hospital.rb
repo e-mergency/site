c8d7aeca58bc7ec33c6788d0bceeacf39996fd04
Simon Funke <news-funsi@gmx.de>
2011-08-14 16:11:52 +0100

    new model functionality: distance function computes the distance to a given location. find_hospitals_near_latlon finds the hospitals in a specified range around the given location

--- app/models/hospital.rb
+++ app/models/hospital.rb
@@ -5,34 +5,42 @@ class Hospital < ActiveRecord::Base
   validates_numericality_of :longitude, :greater_than_or_equal_to => -180.0, :less_than_or_equal_to => 180.0
   validates_numericality_of :latitude, :greater_than_or_equal_to => -90.0, :less_than_or_equal_to => 90.0
 
+  def distance(lat, lon)
+    lat2 = self.latitude
+    lon2 = self.longitude
+    distance = Hospital.distance(lat, lon, lat2, lon2)
+  end
+
+  def self.find_hospitals_near_latlon(lat, lon, max_distance)
+    earth_radius = 6371000.0
+    earth_radius_at_lat = Math.cos(Float(lat)/180*Math::PI)*earth_radius
+
+    # Check for valid input data
+    return [] if max_distance <= 0.0
+    max_distance = [earth_radius, Float(max_distance)].min
+
+    # The hospitals of interest are in the bounding box [longtitude +- delta_max_lat, latitude +- delta_max_lon]
+    delta_max_lon = Float(max_distance)/earth_radius/Math::PI*180
+    delta_max_lat = Float(max_distance)/earth_radius_at_lat/Math::PI*180
+    
+    return Hospital.where(:longitude => (lon-delta_max_lon..lon+delta_max_lon), :latitude => (lat-delta_max_lat..lat+delta_max_lat)) 
+    
+     #Hospital.find_each(:longtitude => (longtitude-delta_max..longtitude+delta_max), :latitude => (latitude-delta_max..latitude+delta_max)) do |hospital|
+  end
+
+  ### Class methods  ###
+
   def self.to_rad(ang)
     rad = ang/180.0*Math::PI
   end
 
-  def self.distance(lon1, lat1, lon2, lat2)
+  def self.distance(lat1, lon1, lat2, lon2)
     # For perfomance reasons use the equirectangular approximation.
     # Its fast and accurate for small distances
-    earth_radius = 6371000
+    earth_radius = 6371000.0
     x = (Hospital.to_rad(lon2-lon1)) * Math.cos((Hospital.to_rad(lat1+lat2))/2)
     y = Hospital.to_rad(lat2-lat1)
     d = Math.sqrt(x*x + y*y) * earth_radius
   end
-
-#  def self.find_hospitals_from_longlat(longtitude, latitude, max_distance)
-#    earth_radius =  6371000
-#
-#    return [] if max_distance <= 0.0
-#    max_distance=earth_radius if max_distance >= earth_radius
-#
-#    # The bounding box:
-#    # The hospitals of interest are in [longtitude +- delta_max, latitude +- delta_max]
-#    delta_max = max_distance/earth_radius/Math::PI*180
-#    
-#    return Hospital.find(:longtitude => (longtitude-delta_max..longtitude+delta_max), :latitude => (latitude-delta_max..latitude+delta_max)) 
-#    
-#      #Hospital.find_each(:longtitude => (longtitude-delta_max..longtitude+delta_max), :latitude => (latitude-delta_max..latitude+delta_max)) do |hospital|
-#    #  debugger
-#    #end
-#  end
 
 end
--- test/unit/hospital_test.rb
+++ test/unit/hospital_test.rb
@@ -34,15 +34,38 @@ class HospitalTest < ActiveSupport::TestCase
     simons = (Hospital.where :name => 'Simons Hospital')[0]
     kates = (Hospital.where :name => 'Kates Hospital')[0]
     peters = (Hospital.where :name => 'Peters Hospital')[0]
+    kushals = (Hospital.where :name => 'Kushals Hospital')[0]
+    florians = (Hospital.where :name => 'Florians Hospital')[0]
 
-    dist = Hospital.distance(simons.longitude, simons.latitude, peters.longitude, peters.latitude)
+    dist = Hospital.distance(simons.latitude, simons.longitude, peters.latitude, peters.longitude)
     assert((dist-5000).abs<1.0)
 
-    dist = Hospital.distance(simons.longitude, simons.latitude, kates.longitude, kates.latitude)
+    dist = simons.distance(peters.latitude, peters.longitude)
+    assert((dist-5000).abs<1.0)
+
+    dist = Hospital.distance(simons.latitude, simons.longitude, kates.latitude, kates.longitude)
     assert((dist-1000).abs<10.0)
 
-    dist = Hospital.distance(kates.longitude, kates.latitude, peters.longitude, peters.latitude)
-    assert((dist-5000).abs<100.0)
+    dist = Hospital.distance(simons.latitude, simons.longitude, florians.latitude, florians.longitude)
+    assert((dist-5000).abs<20.0)
+
+    dist = Hospital.distance(simons.latitude, simons.longitude, kates.latitude, kates.longitude)
+    assert((dist-1000).abs<10.0)
+
+    dist = Hospital.distance(peters.latitude, peters.longitude, florians.latitude, florians.longitude)
+    assert((dist-Math.sqrt(5000**2+5000**2)).abs<15.0)
+
+    dist = Hospital.distance(kushals.latitude, kushals.longitude, florians.latitude, florians.longitude)
+    assert((dist-Math.sqrt(5000**2+10000**2)).abs<5.0)
+
+    dist = Hospital.distance(kates.latitude, kates.longitude, florians.latitude, florians.longitude)
+    assert((dist-6000).abs<25.0)
   end
 
+  test "find_hospital_near" do
+    assert Hospital.find_hospitals_near_latlon(0, 0, 10).count == 0
+    simons = (Hospital.where :name => 'Simons Hospital')[0]
+#    Hospital.find_hospitals_near_latlon(simons.latitude, simons.longitude, 11000).count
+#    Hospital.find_hospitals_near_latlon(simons.latitude, simons.longitude, 11000).count
+  end
 end
