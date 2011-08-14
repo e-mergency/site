14b36b239439141eed7a8745386bd1f0c9421810
Simon Funke <news-funsi@gmx.de>
2011-08-14 11:49:26 +0100

    unit tests for the hospital model

--- test/unit/hospital_test.rb
+++ test/unit/hospital_test.rb
@@ -1,8 +1,29 @@
 require 'test_helper'
 
-class HospitalsTest < ActiveSupport::TestCase
+class HospitalTest < ActiveSupport::TestCase
   # Replace this with your real tests.
   test "the truth" do
     assert true
+  end
+  
+  test "validation" do
+    hospital = Hospital.new 
+    assert !hospital.valid?
+
+    hospital = Hospital.new :name => "Test", :longitude => 0.0, :latitude => -91.0
+    assert !hospital.valid?
+
+    hospital = Hospital.new :name => "Test", :longitude => 0.0, :latitude => 91.0
+    assert !hospital.valid?
+
+    hospital = Hospital.new :name => "Test", :longitude => 181.0, :latitude => 0.0
+    assert !hospital.valid?
+
+    hospital = Hospital.new :name => "Test", :longitude => 181.0, :latitude => 0.0
+    assert !hospital.valid?
+
+    hospital = Hospital.new :name => "Test", :longitude => 18.0, :latitude => 90.0
+    assert hospital.valid?
+    assert hospital.save
   end
 end
