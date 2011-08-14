f3d658777f5cd35ef49d2561c29313ac343bacca
Kushal Pisavadia <kushi.p@gmail.com>
2011-08-14 12:59:32 +0100

    Got a basic scraper working with Nokogiri. Can currently use with 'rails c'. Here's how this is currently being tested:
    $ rails c (inside of site repo root folder)
    >> require 'lib/NHSChoicesAPI.rb'
    => nil
    >> a = NHSChoicesAPI::Scraper.new
    => #<NHSChoicesAPI::Scraper:0x103700068>
    >> a.get_hospital_ids
    => this is where an array with all IDs should be printed

--- lib/NHSChoicesAPI.rb
+++ lib/NHSChoicesAPI.rb
@@ -1,16 +1,31 @@
-module NHSChoicesAPI
-  
-  @NHSAPIKEY = ''
-  def getHospitalIDs
-    json = get 'http://v1.syndication.nhschoices.nhs.uk/organisations/hospitals/identifiermappings'
-    puts json.inspect
-  end
-  
-  def get(url)
-    Nestful.json_get url
-  end
+require 'nokogiri'
+require 'open-uri'
 
-  def apikey
-    return '?apikey=' + @NHSAPIKEY
+module NHSChoicesAPI
+  include Nokogiri
+
+  NHSAPIKEY = 'NOCRIYLM'
+  
+  class Scraper
+    @api_url = 'http://v1.syndication.nhschoices.nhs.uk/organisations/hospitals/'
+
+    def get_hospital_ids
+      # Get hospital id list. Memoize the result to @ID_ARRAY
+      @hospital_ids ||= Scraper.get_hospital_ids_from_server
+    end
+
+    def self.get_hospital_ids_from_server
+      url = @api_url + 'identifiermappings.xhtml' + api_key
+      doc = Nokogiri::HTML(open(url))
+      id_arr = []
+      doc.css('#identifierMappings tr td.id').each do |id|
+        id_arr << id.content
+      end
+      return id_arr
+    end
+    
+    def self.api_key
+      return '?apikey=' + NHSChoicesAPI::NHSAPIKEY
+    end
   end
 end
