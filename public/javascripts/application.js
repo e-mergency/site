b1476493148e0f7d2782f8ab3d5735db6e8ae57c
Kushal Pisavadia <kushi.p@gmail.com>
2011-11-05 14:44:47 +0000

    Clean up JS, to be ready to add Google Maps core JS
    - ran through JSHint to clean any expected errors

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,32 +1,40 @@
-// Place your application-specific JavaScript functions and classes here
-// This file is automatically included by javascript_include_tag :defaults
+// Place your application-specific JavaScript functions and classes
+// here. This file is automatically included by
+// javascript_include_tag :defaults.
 
 var reloadGMapTimer;
+var debug = true;
+
+// prevent console errors on browsers without firebug
+if (!window.console || !debug) {
+  window.console = {};
+  window.console.log = function(){};
+}
+
+function reloadGMap() {
+  console.log("reloading Google map");
+  var mapCanvas = $('#map-canvas');
+  var mapImage = $("gmapStatic");
+  var newGmapImage = "http://maps.googleapis.com/maps/api/staticmap?center=SW61SH,London&zoom=14&size=" + mapImage.width() + "x" + mapImage.height() + "&maptype=roadmap&sensor=false";
+  mapImage.attr('src', newGmapImage);
+}
+
+function fitMapToWindow() {
+  var mapCanvas = $('#map-canvas');
+  var hospitalList = $('#hospital-list');
+  var compensation = 2; //For borders
+  var mapImage = $('#gmapStatic');
+  
+  mapCanvas.width($(window).width() - hospitalList.width() - compensation);
+  
+  //Set the new static map size
+  mapImage.width(mapCanvas.width());
+  
+  clearTimeout(reloadGMapTimer);
+  reloadGMapTimer = setTimeout(reloadGMap(), 1000);
+}
+
 $(document).ready(function() {
-    fitMapToWindow();
+  fitMapToWindow();
+  $(window).resize(fitMapToWindow);
 });
-
-$(window).resize(fitMapToWindow);
-
-function fitMapToWindow(){
-    var mapCanvas = $('#map-canvas');
-    var hospitalList = $('#hospital-list');
-    var compensation = 2; //For borders
-    var mapImage = $('#gmapStatic');
-    
-    mapCanvas.width($(window).width() - hospitalList.width() - compensation);
-    
-    //Set the new static map size
-    mapImage.width(mapCanvas.width());
-    
-    clearTimeout(reloadGMapTimer);
-    reloadGMapTimer = setTimeout('reloadGMap()', 1000); //$(document).oneTime(1000, reloadGMap());
-}
-
-function reloadGMap(){
-    console.log("reloading Google map");
-    var mapCanvas = $('#map-canvas');
-    var mapImage = $('#gmapStatic');
-    var newGmapImage = "http://maps.googleapis.com/maps/api/staticmap?center=SW61SH,London&zoom=14&size=" + mapImage.width() + "x" + mapImage.height() + "&maptype=roadmap&sensor=false";
-    mapImage.attr('src', newGmapImage)
-}
