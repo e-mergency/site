e61f108879d8580fd6d9ec4ee3a4648c862d9695
Florian Rathgeber <f.rathgeber10@imperial.ac.uk>
2011-11-05 15:24:16 +0000

    get rid of map method in Hospitals controller

--- app/controllers/hospitals_controller.rb
+++ app/controllers/hospitals_controller.rb
@@ -4,8 +4,4 @@ class HospitalsController < ApplicationController
   def index
     @hospitals = Hospital.find_hospitals_near_latlon(53.9431092443469, -2.2680899081386, 5000)
   end
-
-  def map
-    @json = Hospital.all.to_json
-  end
 end
