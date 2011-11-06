d708a636161caf4ac4866a9826586e2778a37fe1
Peter Hamilton <peter@inspiredpixel.net>
2011-11-06 18:48:47 +0000

    Can filter hospitals (stubs/method created but needs writing) and map and list get repopulated

--- app/controllers/hospitals_controller.rb
+++ app/controllers/hospitals_controller.rb
@@ -5,8 +5,16 @@ class HospitalsController < ApplicationController
     @hospitals = Hospital.find_hospitals_near_latlon(53.9431092443469, -2.2680899081386, 5000)
   end
   
-  def list_all
-    @hospitals = Hospital.find(:all, :limit => 10)
+  def json_list
+    case params[:sort]
+    when "agony"
+      @hospitals =  Hospital.find(:all, :limit => 20).reverse[0..9]
+    when "wait"
+      @hospitals =  Hospital.find(:all, :limit => 30).reverse[0..9]
+    else
+      @hospitals = Hospital.find(:all, :limit => 10)
+    end
+    puts @hospitals.inspect
     render :json => @hospitals
   end
 end
