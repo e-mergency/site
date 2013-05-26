53df64347b56efcc09eaf4bd9fcfcab3fae3d1bb
Florian Rathgeber <florian.rathgeber@gmail.com>
2013-05-26 14:37:38 +0100

    Fix delays controller

--- app/controllers/delays_controller.rb
+++ app/controllers/delays_controller.rb
@@ -18,11 +18,11 @@ class DelaysController < ApplicationController
   # GET /delays
   # GET /delays.xml
   def index
-    nb_days = 7
-    start = nb_days.days.ago 
-    dates = [start.to_f] + Delay.all(:conditions => {:created_at => start..0.days.ago, :hospital_id => get_hospital.id}).map{|d| d.created_at.to_f}.reverse + [0.days.ago.to_f] # We add to additional nodes at the beginning and the end of the timespan
+    start = 7.days.ago
+    delays = @hospital.delays.where(:created_at => start..0.days.ago)
+    dates = [start.to_f] + delays.map{|d| d.created_at.to_f}.reverse + [0.days.ago.to_f] # We add to additional nodes at the beginning and the end of the timespan
     dates2 = dates.map{|d| (d-start.to_f)/20}
-    delays = Delay.all(:conditions => {:created_at => start..0.days.ago, :hospital_id => get_hospital.id}, :select => :minutes).map(&:minutes).reverse
+    delays = delays.map(&:minutes).reverse
     if delays.empty?
       delays2 = [0.0, 0.1]
     else
@@ -30,12 +30,12 @@ class DelaysController < ApplicationController
     end
     dates2 = dates2.collect { |d| d * delays2.max / dates2.max if dates2.max > 0 }
     data = [dates2, delays2]
-    wdays = ['Sun', 'Sat', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']
+    wdays = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']
     (0..Date.today.wday+1).each do |i|
       d = wdays.shift
       wdays.push(d)
     end
-    @graph_url = Gchart.line_xy(:size => '500x300', 
+    @graph_url = Gchart.line_xy(:size => '500x300',
                              :title => "Last weeks waiting time",
                              :data => data,
                              :axis_with_label => 'x,y',
