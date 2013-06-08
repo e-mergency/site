48e383cbae02a0bfb931cfd9ab1075e495fa2891
Florian Rathgeber <florian.rathgeber@gmail.com>
2013-06-08 02:34:14 +0100

    Setup method also empties collections other than system.indexes

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,6 +4,9 @@ require 'rails/test_help'
 
 class ActiveSupport::TestCase
   def setup
+    # Empty collections other than system.indexes
+    Mongoid.default_session.collections.select {|c| c.name !~ /system/ }.each {|c| c.find.remove_all}
+
     @simons = FactoryGirl.create(:simons)
     @kates = FactoryGirl.create(:kates)
     @peters = FactoryGirl.create(:peters)
