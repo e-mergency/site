f26145c6d2a9e99075ce9c3fe6c3a5e16e01a97b
Florian Rathgeber <florian.rathgeber@gmail.com>
2013-06-08 02:33:43 +0100

    Add setup method to create factories

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,4 +3,11 @@ require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 
 class ActiveSupport::TestCase
+  def setup
+    @simons = FactoryGirl.create(:simons)
+    @kates = FactoryGirl.create(:kates)
+    @peters = FactoryGirl.create(:peters)
+    @kushals = FactoryGirl.create(:kushals)
+    @florians = FactoryGirl.create(:florians)
+  end
 end
